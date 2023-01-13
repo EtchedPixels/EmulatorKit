4402786443e22753b5bc4702ce0be3f8f588d3c1
Alan Cox <etchedpixels@gmail.com>
2023-01-13 23:54:51 +0000

    68knano: add system

---
+++ 68knano.c
@@ -0,0 +1,383 @@
+/*
+ *	Emulator for Matt Sarnoff's 68K nano. Does not currently emulate the
+ *	bitbang SPI RTC
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <m68k.h>
+#include "16x50.h"
+#include "ide.h"
+
+/* IDE controller */
+static struct ide_controller *ide;
+/* Serial */
+static struct uart16x50 *uart;
+/* 1MB RAM */
+static uint8_t ram[0x100000];
+/* 64K ROM */
+static uint8_t rom[0x10000];
+static int trace = 0;
+
+#define TRACE_MEM	1
+#define TRACE_CPU	2
+#define TRACE_UART	4
+
+uint8_t fc;
+
+/* Read/write macros */
+#define READ_BYTE(BASE, ADDR) (BASE)[ADDR]
+#define READ_WORD(BASE, ADDR) (((BASE)[ADDR]<<8) | \
+			(BASE)[(ADDR)+1])
+#define READ_LONG(BASE, ADDR) (((BASE)[ADDR]<<24) | \
+			((BASE)[(ADDR)+1]<<16) | \
+			((BASE)[(ADDR)+2]<<8) | \
+			(BASE)[(ADDR)+3])
+
+#define WRITE_BYTE(BASE, ADDR, VAL) (BASE)[ADDR] = (VAL)&0xff
+#define WRITE_WORD(BASE, ADDR, VAL) (BASE)[ADDR] = ((VAL)>>8) & 0xff; \
+			(BASE)[(ADDR)+1] = (VAL)&0xff
+#define WRITE_LONG(BASE, ADDR, VAL) (BASE)[ADDR] = ((VAL)>>24) & 0xff; \
+			(BASE)[(ADDR)+1] = ((VAL)>>16)&0xff; \
+			(BASE)[(ADDR)+2] = ((VAL)>>8)&0xff; \
+			(BASE)[(ADDR)+3] = (VAL)&0xff
+
+unsigned int check_chario(void)
+{
+	fd_set i, o;
+	struct timeval tv;
+	unsigned int r = 0;
+
+	FD_ZERO(&i);
+	FD_SET(0, &i);
+	FD_ZERO(&o);
+	FD_SET(1, &o);
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
+
+	if (select(2, &i, &o, NULL, &tv) == -1) {
+		perror("select");
+		exit(1);
+	}
+	if (FD_ISSET(0, &i))
+		r |= 1;
+	if (FD_ISSET(1, &o))
+		r |= 2;
+	return r;
+}
+
+unsigned int next_char(void)
+{
+	char c;
+	if (read(0, &c, 1) != 1) {
+		printf("(tty read without ready byte)\n");
+		return 0xFF;
+	}
+	return c;
+}
+
+static unsigned int irq_pending;
+
+void recalc_interrupts(void)
+{
+	/*  UART autovector 1 */
+	if (uart16x50_irq_pending(uart))
+		m68k_set_irq(M68K_IRQ_1);
+	else
+		m68k_set_irq(0);
+}
+
+int cpu_irq_ack(int level)
+{
+	/* TODO */
+	return M68K_INT_ACK_SPURIOUS;
+}
+
+/* Read data from RAM, ROM, or a device */
+unsigned int do_cpu_read_byte(unsigned int address, unsigned int trap)
+{
+	address &= 0xFFFFFF;
+	switch(address >> 20 ) {
+	case 0x00:	/* ROM */
+	case 0x02:
+		return rom[address & 0xFFFF];
+	case 0x08:	/* Open bus */
+		return 0xFF;
+	case 0x09:
+		return ide_read8(ide, (address & 0x1E) >> 1);
+	case 0x0A:
+		return uart16x50_read(uart, (address & 0x1E) >> 1);
+	case 0x0C:
+	case 0x0E:
+		return ram[address & 0xFFFFF];
+	default:
+		if (trap) {
+			fprintf(stderr, "0x%06X address conflict.\n", address);
+			exit(1);
+		}
+		return 0xFF;	/* For disassembly */
+	}
+	return 0xFF;
+}
+
+unsigned int cpu_read_byte(unsigned int address)
+{
+	unsigned int v = do_cpu_read_byte(address, 1);
+	if (trace & TRACE_MEM)
+		fprintf(stderr, "RB %06X -> %02X\n", address, v);
+	return v;
+}
+
+unsigned int do_cpu_read_word(unsigned int address, unsigned int trap)
+{
+	return (do_cpu_read_byte(address, trap) << 8) | do_cpu_read_byte(address + 1, trap);
+}
+
+unsigned int cpu_read_word(unsigned int address)
+{
+	unsigned int v = do_cpu_read_word(address, 1);
+	if (trace & TRACE_MEM)
+		fprintf(stderr, "RW %06X -> %04X\n", address, v);
+	return v;
+}
+
+unsigned int cpu_read_word_dasm(unsigned int address)
+{
+	return do_cpu_read_word(address, 0);
+}
+
+unsigned int cpu_read_long(unsigned int address)
+{
+	return (cpu_read_word(address) << 16) | cpu_read_word(address + 2);
+}
+
+unsigned int cpu_read_long_dasm(unsigned int address)
+{
+	return (cpu_read_word_dasm(address) << 16) | cpu_read_word_dasm(address + 2);
+}
+
+void cpu_write_byte(unsigned int address, unsigned int value)
+{
+	address &= 0xFFFFFF;
+	switch(address >> 20 ) {
+	case 0x00:	/* ROM */
+	case 0x02:
+		if (trace & TRACE_MEM)
+			fprintf(stderr,  "%06x: write to ROM.\n", address);
+		return;
+	case 0x08:	/* Open bus */
+		return;
+	case 0x09:
+		ide_write8(ide, (address & 0x1E) >> 1, value);
+		return;
+	case 0x0A:
+		uart16x50_write(uart, (address & 0x1E) >> 1, value);
+		return;
+	case 0x0C:
+	case 0x0E:
+		ram[address & 0xFFFFF] = value;
+		return;
+	default:
+		fprintf(stderr, "0x%06X address conflict.\n", address);
+		exit(1);
+	}
+}
+
+void cpu_write_word(unsigned int address, unsigned int value)
+{
+	address &= 0xFFFFFF;
+
+	if (trace & TRACE_MEM)
+		fprintf(stderr, "WW %06X <- %04X\n", address, value);
+
+	cpu_write_byte(address, value >> 8);
+	cpu_write_byte(address + 1, value & 0xFF);
+}
+
+void cpu_write_long(unsigned int address, unsigned int value)
+{
+	address &= 0xFFFFFF;
+
+	cpu_write_word(address, value >> 16);
+	cpu_write_word(address + 2, value & 0xFFFF);
+}
+
+void cpu_write_pd(unsigned int address, unsigned int value)
+{
+	address &= 0xFFFFFF;
+
+	cpu_write_word(address + 2, value & 0xFFFF);
+	cpu_write_word(address, value >> 16);
+}
+
+void cpu_instr_callback(void)
+{
+	if (trace & TRACE_CPU) {
+		char buf[128];
+		unsigned int pc = m68k_get_reg(NULL, M68K_REG_PC);
+		m68k_disassemble(buf, pc, M68K_CPU_TYPE_68000);
+		fprintf(stderr, ">%06X %s\n", pc, buf);
+	}
+}
+
+static void device_init(void)
+{
+	irq_pending = 0;
+	ide_reset_begin(ide);
+	uart16x50_reset(uart);
+	uart16x50_set_input(uart, 1);
+}
+
+static struct termios saved_term, term;
+
+static void cleanup(int sig)
+{
+	tcsetattr(0, 0, &saved_term);
+	exit(1);
+}
+
+static void exit_cleanup(void)
+{
+	tcsetattr(0, 0, &saved_term);
+}
+
+static void take_a_nap(void)
+{
+	struct timespec t;
+	t.tv_sec = 0;
+	t.tv_nsec = 100000;
+	if (nanosleep(&t, NULL))
+		perror("nanosleep");
+}
+
+void cpu_pulse_reset(void)
+{
+	device_init();
+}
+
+void cpu_set_fc(int fc)
+{
+}
+
+void usage(void)
+{
+	fprintf(stderr, "68knano: [-0][-1][-2][-e][-r rompath][-i idepath][-d debug].\n");
+	exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	int cputype = M68K_CPU_TYPE_68000;
+	int fast = 0;
+	int opt;
+	const char *romname = "68knano.rom";
+	const char *diskname = "68knano.ide";
+
+	while((opt = getopt(argc, argv, "012efd:i:r:")) != -1) {
+		switch(opt) {
+		case '0':
+			cputype = M68K_CPU_TYPE_68000;
+			break;
+		case '1':
+			cputype = M68K_CPU_TYPE_68010;
+			break;
+		case '2':
+			cputype = M68K_CPU_TYPE_68020;
+			break;
+		case 'e':
+			cputype = M68K_CPU_TYPE_68EC020;
+			break;
+		case 'f':
+			fast = 1;
+			break;
+		case 'd':
+			trace = atoi(optarg);
+			break;
+		case 'i':
+			diskname = optarg;
+			break;
+		case 'r':
+			romname = optarg;
+			break;
+		default:
+			usage();
+		}
+	}
+
+	if (tcgetattr(0, &term) == 0) {
+		saved_term = term;
+		atexit(exit_cleanup);
+		signal(SIGINT, SIG_IGN);
+		signal(SIGQUIT, cleanup);
+		signal(SIGTSTP, SIG_IGN);
+		term.c_lflag &= ~ICANON;
+		term.c_iflag &= ~(ICRNL | IGNCR);
+		term.c_cc[VMIN] = 1;
+		term.c_cc[VTIME] = 0;
+		term.c_cc[VINTR] = 0;
+		term.c_cc[VSUSP] = 0;
+		term.c_cc[VEOF] = 0;
+		term.c_lflag &= ~(ECHO | ECHOE | ECHOK);
+		tcsetattr(0, 0, &term);
+	}
+
+	if (optind < argc)
+		usage();
+
+	memset(ram, 0xA7, sizeof(ram));
+
+	fd = open(romname, O_RDONLY);
+	if (fd == -1) {
+		perror(romname);
+		exit(1);
+	}
+	if (read(fd, rom, 0x10000) < 0x10000) {
+		fprintf(stderr, "%s: too short.\n", romname);
+		exit(1);
+	}
+	close(fd);
+
+	fd = open(diskname, O_RDWR);
+	if (fd == -1) {
+		perror(diskname);
+		exit(1);
+	}
+	ide = ide_allocate("hd0");
+	if (ide == NULL)
+		exit(1);
+	if (ide_attach(ide, 0, fd))
+		exit(1);
+
+	uart = uart16x50_create();
+	if (trace & TRACE_UART)
+		uart16x50_trace(uart, 1);
+
+	m68k_init();
+	m68k_set_cpu_type(cputype);
+	m68k_pulse_reset();
+
+	/* Init devices */
+	device_init();
+
+	while (1) {
+		/* A 10MHz 68000 should do 1000 cycles per 1/10000th of a
+		   second. We do a blind 0.01 second sleep so we are actually
+		   emulating a bit under 10Mhz - which will do fine for
+		   testing this stuff */
+		m68k_execute(1000);
+		uart16x50_event(uart);
+		if (!fast)
+			take_a_nap();
+	}
+}
--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ all:	rc2014 rc2014-1802 rc2014-6303 rc2014-6502 rc2014-65c816-mini \
 	rc2014-65c816 rc2014-6800 rc2014-68008 rc2014-6809 rc2014-68hc11 \
 	rc2014-80c188 rc2014-8085 rc2014-z8 rc2014-z180 rbcv2 searle linc80 \
 	makedisk markiv mbc2 smallz80 sbc2g z80mc simple80 flexbox tiny68k \
-	s100-z80 scelbi rb-mbc rc2014-tms9995 rhyophyre pz1
+	s100-z80 scelbi rb-mbc rc2014-tms9995 rhyophyre pz1 68knano
 
 sdl2:	rc2014_sdl2 nc100 nc200 n8_sdl2 scelbi_sdl2 nascom uk101 z180-mini-itx_sdl2 vz300
 
@@ -120,6 +120,12 @@ tiny68k: tiny68k.o ide.o duart.o m68k/lib68k.a
 
 tiny68k.o: tiny68k.c m68k/lib68k.a
 	$(CC) $(CFLAGS) -Im68k -c tiny68k.c
+
+68knano: 68knano.o ide.o 16x50.o m68k/lib68k.a
+	cc -g3 68knano.o ide.o 16x50.o m68k/lib68k.a -o 68knano
+
+68knano.o: 68knano.c m68k/lib68k.a
+	$(CC) $(CFLAGS) -Im68k -c 68knano.c
 
 z80mc:	z80mc.o sdcard.o libz80/libz80.o
 	cc -g3 z80mc.o sdcard.o libz80/libz80.o -o z80mc
