c54364ee98140fc7dc7d9d5a02c3e568be73c7ab
Ralph Irving <ralph.irving@gmail.com>
2023-06-08 08:17:52 -0400

    2063: fix building without joystick support.

--- nojoystick.c
+++ nojoystick.c
@@ -2,18 +2,17 @@
 #include <stdbool.h>
 #include "joystick.h"
 
-bool Joystick_slots_enabled[NUM_JOYSTICKS] = {false, false};
+bool Joystick_slots_enabled[NUM_JS] = {false, false};
 
-bool joystick_create(void)
+void joystick_create(void)
 {
-	return false;
 }
 
-uint8_t joystick_read(uint8_t i)
+uint8_t joystick_read(int i)
 {
 	return 0x78;
 }
 
-void joystick_trace(bool enable)
+void joystick_trace(unsigned enable)
 {
 }
